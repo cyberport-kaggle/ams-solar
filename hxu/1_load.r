4b11244fafa109ad6bdabf9a7f63ba408d7092da
Han Xu <hgcrpd@gmail.com>
2013-07-30 22:17:55 +0800

    helper functions

--- hxu/1_load.r
+++ hxu/1_load.r
@@ -13,6 +13,7 @@ trainFolder <- 'train/' # Path to train files
 
 stationInfo <- read.csv(paste(dataFolder, 'station_info.csv', sep=''))
 stationNames <- stationInfo$stid
+knownDims <- c('lat', 'lon', 'ens', 'fhour', 'intValidTime', 'intTime', 'time')
 
 # Names of the netCDF files that contain the data
 # The following are all of the variables provided
@@ -37,6 +38,59 @@ testFiles <- read.csv('test_filenames.txt', header=FALSE)[[1]]
 
 # In R, each of the factors should have dimensions of (lon, lat, fhour, ens, time)
 # This is reversed from netCDF, and what the python code will read
+
+getDimensions <- function(nc) {
+    # Returns a list of the dimensions and their values from the netcdf file.
+    # This is so that we don't have to continue to refer to dimensions just by their index
+    # And for paranoid checking that the dimensions are in the right order in each file
+
+    res <- list()
+    res$lon <- ncvar_get(nc, 'lon')
+    res$lat <- ncvar_get(nc, 'lat')
+    res$fhour <- ncvar_get(nc, 'fhour')
+    res$ens <- ncvar_get(nc, 'ens') # ensemble numbers; 0-10
+    res$intTime <- ncvar_get(nc, 'intTime')
+    return(res)
+}
+
+getVarName <- function(nc) {
+    # Variable names are different in each package, so we need to read it out.
+    # Since we know all of the other variable names, we just eliminate them and the only
+    # one left is the variable we want
+    varNames <- c(names(nc$dim), names(nc$var))
+    res <- varNames[!varNames %in% knownDims]
+    if (length(res) == 1) {
+        return(res)
+    } else {
+        warning(paste("Could not find the variable name in ", nc$filename, ".\nRemaining names are ", paste(res, collapse=" "), sep=""))
+        return(NULL)
+    }
+}
+
+getPoints <- function(lat, lon, dims, n=1) {
+    # Given a latitude and longitude, find the n nearest points in the GEFS grid 
+    # in each direction
+    # Parameters:
+    # @lat: The latitude of the Mesonet location
+    # @lon: the longitude of the Mesonet location
+    # @n: number of points in each direction to find.  So n=1 should return four points
+    # @dims: the list returned by getDimensions for the file
+    # 
+    # Returns the points as a start index in the netcdf file and a count
+    lats <- dims$lat
+    lons <- dims$lon
+    # Since GEFS is spaced in 1 degree intervals, multiply by n to get the max distance in degrees from the lat and lon
+    maxDist <- 1 * n
+    # station values are negative values, so we add 360 to get it in the same scale as the GEFS coordinates
+    if (lon < 0) {
+        lon <- lon + 360
+    }
+    latIdx <- which(abs(lats - lat) <= maxDist)
+    lonIdx <- which(abs(lons - lon) <= maxDist)
+    cat(paste("Using GEFS points at lats: ", paste(lats[latIdx], collapse=" "), " and lons: ", paste(lons[lonIdx], collapse=" "), sep=""))
+    return(list(latStart=latIdx[1], latCnt=n*2, lonStart=lonIdx[1], lonCnt=n*2))
+}
+
 
 ##########
 # Load data
